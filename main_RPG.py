fb0287481dff2b70bc7b8faf5a41c02952aa003e
Nathan-Seeley <wntrstk111@gmail.com>
2022-10-23 22:49:46 -0400

    Changed casing on rpg_print_statements file and created variables for a hero and three enemies. Names, equipment, and attacks are pending.

--- main_RPG.py
+++ main_RPG.py
@@ -1,7 +1,5 @@
 # import rpg_functions
 # from colorama import Fore 
 
-# my_blue_string = f"{Fore.BLUE} "
-# my_green_string = f"{Fore.GREEN} "
-# my_red_string = f"{Fore.RED} "
+
 
---
+++ rpg_variables.py
@@ -0,0 +1,58 @@
+"""color of print variables
+
+my_blue_string = f"{Fore.BLUE} "
+my_green_string = f"{Fore.GREEN} "
+my_red_string = f"{Fore.RED} "
+
+"""
+my_hero = {
+    "name" : "",
+    "level" : 1,
+    "health" : 100,
+    "equipment" : {"",""},
+    "attacks" : (("",50), ("",100), ("",150)),
+    "coins" : {
+        "copper" : 9,
+        "silver" : 3,
+        "gold" : 0,    
+    }
+}
+
+enemy_one = {
+    "name" : "",
+    "level" : 1,
+    "health" : 100,
+    "equipment" : {"", ""},
+    "attacks" : (("",15), ("",65)("", 115)),
+    "coins" : {
+        "copper" : 12,
+        "silver" : 6,
+        "gold" : 3,
+    }
+}
+
+enemy_two = {
+    "name" : "",
+    "level" : 2,
+    "health" : 1115,
+    "equipment" : {"", ""},
+    "attacks" : (("",30), ("",80), ("",130)),
+    "coins" : {
+        "copper" : 15,
+        "silver" : 9,
+        "gold" : 6
+    }
+}
+
+enemy_three = {
+    "name" : "",
+    "level" : 3,
+    "health" : 1115,
+    "equipment" : {"", ""},
+    "attacks" : (("",45), ("",95), ("",145)),
+    "coins" : {
+        "copper" : 18,
+        "silver" : 12,
+        "gold" : 9
+    }
+}
