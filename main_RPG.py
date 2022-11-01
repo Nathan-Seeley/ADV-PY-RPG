dbd41fde3822c603da1b406ea1d67af4caaf0b03
Nathan-Seeley <wntrstk111@gmail.com>
2022-11-01 16:04:08 -0400

    Organized my main run_battle file. Found and created a way to use colorama without getting an error/appi code. changed some story line print outs to make battle more kid friendly.

--- main_RPG.py
+++ main_RPG.py
@@ -3,10 +3,23 @@ import rpg_functions
 from rpg_print_statements import introduction_sequence
 
 def run_battle (hero,enemies):
-    # rpg_functions.story (introduction_sequence)
-    for enemy in enemies:
-        # rpg_functions.hero_vs_enemy_battle (hero, enemy)
-        # rpg_functions.equipment_loot (hero, enemy)
-        rpg_functions.coin_collection (hero, enemy)
+    rpg_functions.story_teller (introduction_sequence)
+    while hero ["health"] > 0:
+        for enemy in enemies:
+            if hero["health"] > 0:
+                rpg_functions.random_battle_generator (hero, enemy)
+                rpg_functions.loot_collector (hero, enemy)
+                rpg_functions.coin_collector (hero, enemy)
+                rpg_functions.level_increaser (hero)
+                rpg_functions.new_attacks_generatror (hero)
+                rpg_functions.health_increaser (hero)
+            elif hero ["health"] < 1:
+                print ("OH NO!!! {hero ['name']} was chased out of the house.") 
+                print ("Going into a house without permission may not be the best idea?") 
+                break
+        if hero ["health"] > 0:
+            print (f"{hero ['name']} can now enjoy all that is JUUUUUST RIGHT!")
+                
     return run_battle
 battle = run_battle (my_hero, my_enemies)
+
--- rpg_functions.py
+++ rpg_functions.py
@@ -1,6 +1,5 @@
-
 import random
-from colorama import Fore, init 
+from colorama import Fore, init
 init ()
 import sys, time
 
@@ -12,50 +11,71 @@ def slow_print (string_to_print):
         time.sleep (0.05)
     print ("")
 
-def story (introduction):
+def story_teller (introduction):
+    print (f"{Fore.CYAN}Welcome to a childrens story adventure.")
     for line in introduction:
-        slow_print (f"{Fore.LIGHTYELLOW_EX} {line}")
+        slow_print (line)
     
-def hero_vs_enemy_battle (hero , enemy):
-    while hero ["health"] and enemy ["health"] >= 1:
-        
-            hero_attack = random.choice (hero ["attacks"])
-            slow_print (f"{Fore.GREEN} {enemy ['name']} takes {hero_attack [1]} damage from {hero ['name']} while using {hero_attack [0]} attack!")
-            enemy ["health"] -= hero_attack [1]
-            if enemy ["health"] <= 0:
-                slow_print (f"{Fore.GREEN} {enemy ['name']} has died! Congratulations!")
-                break
-            elif enemy ["health"] >= 1:
-                slow_print (f"{Fore.LIGHTYELLOW_EX} {enemy ['name']} has {enemy ['health']} health left!")
-            enemy_attack = random.choice (enemy ["attacks"])
-            slow_print (f"{Fore.RED} {hero ['name']} takes {enemy_attack [1]} damage from {enemy ['name']} while using {enemy_attack [0]} attack!")
-            hero ["health"] -= enemy_attack [1]
+def random_battle_generator (hero , enemy):
+    while hero ["health"] and enemy ["health"]> 0:
+        hero_attack = random.choice (hero ["attacks"])
+        print (f"{Fore.GREEN}")
+        slow_print (f"{enemy ['name']} takes {hero_attack [1]} energy drain points from {hero ['name']} while using {hero_attack [0]}!")
+        enemy ["health"] -= hero_attack [1]
             
-            if hero ["health"] <= 0:
-                slow_print (f"{Fore.RED} {hero ['name']} has died. Try again if you dare!")
-                break
-            elif hero ["health"] >=1:
-                slow_print (f"{Fore.LIGHTYELLOW_EX} {hero ['name']} has {hero ['health']} health left!")
-                
-def equipment_loot (hero, enemy):
+        if enemy ["health"] <= 0:
+            print (f"{Fore.RED}")
+            slow_print (f"{enemy ['name']} has fallen asleep!")
+            break
+        elif enemy ["health"] >= 1:
+            print (f"{Fore.LIGHTYELLOW_EX}")
+            slow_print (f"{enemy ['name']} has {enemy ['health']} energy points left!")
+            
+        enemy_attack = random.choice (enemy ["attacks"])
+        print (f"{Fore.RED}")
+        slow_print (f"{hero ['name']} takes {enemy_attack [1]} energy points from {enemy ['name']} while using {enemy_attack [0]}!")
+        hero ["health"] -= enemy_attack [1]
+        if hero ["health"] >0:
+            print (f"{Fore.LIGHTYELLOW_EX}")
+            slow_print (f"{hero ['name']} has {hero ['health']} energy points left!")
+        elif hero ["health"] < 0:
+            break
+
+def loot_collector (hero, enemy):
     if hero ["health"] > 0:
-        slow_print (f"{Fore.GREEN} {hero ['name']} has {hero ['equipment']}.")
-        slow_print (f"{Fore.GREEN} {hero ['name']} has gained {enemy ['equipment']} from {enemy ['name']}!")
+        print (f"{Fore.GREEN}")
+        slow_print (f"{hero ['name']} is equiped with {hero ['equipment']}.")
+        slow_print (f"{hero ['name']} has gained {enemy ['equipment']} from {enemy ['name']}!")
         hero ["equipment"] = hero ["equipment"].union (enemy ["equipment"])
     
-
-def coin_collection (hero, enemy):
+def coin_collector (hero, enemy):
     if hero ["health"] > 0:
-        slow_print (f"{Fore.LIGHTYELLOW_EX} {hero['name']} has {hero ['coins']['copper']} copper, {hero ['coins']['silver']} silver and {hero ['coins']['gold']} gold coins")
-        slow_print (f"{Fore.GREEN} {hero['name']} has gained {enemy ['coins']['copper']} copper, {enemy ['coins']['silver']} silver and {enemy ['coins']['gold']} gold from {enemy ['name']}!")
+        print (f"{Fore.LIGHTYELLOW_EX}") 
+        slow_print (f"{hero['name']} has {hero ['coins']['copper']} copper, {hero ['coins']['silver']} silver and {hero ['coins']['gold']} gold coins")
+        slow_print (f"{hero['name']} has gained {enemy ['coins']['copper']} copper, {enemy ['coins']['silver']} silver and {enemy ['coins']['gold']} gold from {enemy ['name']}!")
         hero ['coins']['copper'] = ((hero ['coins']['copper']) + (enemy ['coins'] ['copper']))
         hero ['coins']['silver'] = ((hero ['coins']['silver']) + (enemy ['coins'] ['silver']))
         hero ['coins']['gold'] = ((hero ['coins']['gold']) + (enemy ['coins'] ['gold']))
-     
-        
-    
-    
 
+def level_increaser (hero):
+    if hero ["health"] > 0:
+        hero ["level"] = hero ["level"] + 1
+        print (f"{Fore.GREEN}")
+        slow_print (f"{hero ['name']} has increased to level {hero ['level']}!")
 
-    
-      
+def new_attacks_generatror (hero):
+    if hero ["health"] > 0:
+        attack_strength = (50, 75, 100, 150)
+        random_attack_strength = random.choice (attack_strength)
+        print (f"{Fore.LIGHTYELLOW_EX}")
+        user_attack_name = (input (f"{hero['name']} has learned a new attack! Name the attack. "))
+        new_attack_list = [(str(user_attack_name),random_attack_strength)]
+        new_attack_tuple = tuple (new_attack_list)
+        hero ["attacks"]  = hero ["attacks"] + new_attack_tuple
+        slow_print (f"{hero['name']} attacks are {hero['attacks']}.")
+def health_increaser (hero):
+    if hero ["health"] > 0:     
+        hero["health"] += 15
+        print (f"{Fore.LIGHTGREEN_EX}")
+        slow_print (f"{hero['name']} has gained 15 health.")
+        slow_print (f"{hero['name']} now has {hero['health']} health.")
